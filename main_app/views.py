48a8eb97d7e5e478d549acc5ec2ca440c3e6eeb6
jharreldesign <harrel.jason@gmail.com>
2024-12-04 09:13:58 -0800

    new routes for cat IDs

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Cat
 
-from django.http import HttpResponse
+def cat_index(request):
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', {'cats': cats})
+    
 
 def home(request):
     return render(request, 'home.html')
@@ -8,21 +12,8 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-def cat_index(request):
-    # Render the cats/index.html template with the cats data
-    return render(request, 'cats/index.html', {'cats': cats})
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
 
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
 
-# Create a list of Cat instances
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
