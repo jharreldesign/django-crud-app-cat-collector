0a7a3508601c928f08ddfa5f1583bb99a8e070b7
jharreldesign <harrel.jason@gmail.com>
2024-12-05 10:20:24 -0800

    made updates to the views and index files

--- main_app/views.py
+++ main_app/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
 from .models import Cat
 
-def cat_index(request):
-    cats = Cat.objects.all()
-    return render(request, 'cats/index.html', {'cats': cats})
-    
-
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def cat_index(request):
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', {'cats': cats})
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
